826cd19e84a6b134228e60962cd4c8380e33db34
Ezequiel Freire <ezequiel.a.freire@gmail.com>
2018-11-16 23:49:54 -0300

    se crea models - mod login - boton style

---
+++ src/app/models/ePerfil.ts
@@ -0,0 +1,5 @@
+export enum Perfil {
+  'profesional',
+  'normal',
+  'free'
+}
---
+++ src/app/models/usuario.ts
@@ -0,0 +1,8 @@
+import { Perfil } from './ePerfil';
+
+export interface Usuario {
+  mail: string;
+  nombre: string;
+  clave: string;
+  perfil: Perfil;
+}
